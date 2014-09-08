3694902e2135cd8a407da26faead7f588645ceac
Michi <mike@lew42.com>
2014-09-08 15:39:10 -0500

    Add build example

---
+++ build/js/app.1bcbb43b.js
@@ -0,0 +1,1 @@
+function test3(){console.log("test3")}!function(){console.log("test1")}(void 0),function(){console.log("test2")}();
---
+++ build/js/test2.02fb00e7.js
@@ -0,0 +1,2 @@
+// test2.js
+;(function(){console.log('test2');})();
